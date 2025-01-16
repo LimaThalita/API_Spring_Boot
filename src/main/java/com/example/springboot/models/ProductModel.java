bca3ac6f98757b496cd665a312090924643f992b
ThalitaEpifanio <thalita.lima@a.ficr.edu.br>
2025-01-16 11:48:59 -0300

    primeiro commit do projeto

---
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -0,0 +1,81 @@
+package com.example.springboot.models;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_PRODUCTS")
+public class ProductModel implements Serializable{
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idProduct;
+    private String nome;
+    private int cpu;
+    private int memoria;
+    private int disco;
+    private LocalDateTime dataCriacao;
+
+    // Getter e Setter para idProduct
+    public UUID getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(UUID idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    // Getter e Setter para nome
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Getter e Setter para cpu
+    public int getCpu() {
+        return cpu;
+    }
+
+    public void setCpu(int cpu) {
+        this.cpu = cpu;
+    }
+
+    // Getter e Setter para memoria
+    public int getMemoria() {
+        return memoria;
+    }
+
+    public void setMemoria(int memoria) {
+        this.memoria = memoria;
+    }
+
+    // Getter e Setter para disco
+    public int getDisco() {
+        return disco;
+    }
+
+    public void setDisco(int disco) {
+        this.disco = disco;
+    }
+
+    // Getter e Setter para dataCriacao
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+}
