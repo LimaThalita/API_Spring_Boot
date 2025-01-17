fbb8d3d6654bc7e0eb2790a70b55af5d66a1d1b3
ThalitaEpifanio <thalita.lima@a.ficr.edu.br>
2025-01-16 21:16:20 -0300

    endpoints corrigidos

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -23,9 +23,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
-
-
-
 @RestController
 public class ProductController {
 
@@ -43,6 +40,7 @@ public class ProductController {
     @GetMapping("/products")
     public ResponseEntity<List<ProductModel>> getAllProducts() {
         return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+        
     }
     
     @GetMapping("/products/{id}")
